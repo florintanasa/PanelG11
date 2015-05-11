fc609daef3f24d6bbc03848874e56b9a365c7b9c
florintanasa <florin.tanasa@gmail.com>
2015-05-11 22:36:44 +0300

    added encoder code

--- PanelG11.ino
+++ PanelG11.ino
@@ -9,6 +9,7 @@
 || #
 */
 #include <Keypad.h>
+#include <Encoder.h>
 
 /*
 160m  80m        60m          Up_filter
@@ -16,17 +17,18 @@
  17m  15m        12m          Step_freq_up
  10m  Mode_left  Mode_right   Step_freq_down
 */
+Encoder myEnc(5,6); // initialize the rotary encoder
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
-  {KEY_0,  KEY_1,       KEY_2,        KEY_PAGE_UP},
-  {KEY_3,  KEY_4,       KEY_5,        KEY_PAGE_DOWN},
+  {KEY_0,  KEY_1,       KEY_2,        KEY_RIGHT},
+  {KEY_3,  KEY_4,       KEY_5,        KEY_LEFT},
   {KEY_6,  KEY_7,       KEY_8,        KEY_UP},
   {KEY_9,  KEY_COMMA,	KEY_PERIOD,   KEY_DOWN}
 };
 byte rowPins[ROWS] = {3, 2, 1, 0}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+byte colPins[COLS] = {9, 8, 7, 4}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -35,6 +37,9 @@ void setup(){
   keypad.setDebounceTime(20); //set debounce in ms
   keypad.setHoldTime(500); //set hold time in ms
 }
+
+long oldPosition = -999;
+int enclevel = 0;
 
 void loop() {
    char key = keypad.getKey(); //read the keys
@@ -46,5 +51,26 @@ void loop() {
   Keyboard.set_key1(0);
   
   Keyboard.send_now();
+  
+  	// rotary encoder stuff
+	long newPosition = myEnc.read();
+	if (newPosition != oldPosition) {
+		int mod = newPosition % 4;
+		if(mod == 0) { // four positions per detent, only want one, hence modulo
+			if(newPosition < enclevel) { // "bounce" preventer
+				Keyboard.set_key1(KEY_PAGE_UP);
+				Keyboard.send_now();
+				Keyboard.set_key1(0);
+				Keyboard.send_now();
+			} else if(newPosition > enclevel) {
+				Keyboard.set_key1(KEY_PAGE_DOWN);
+				Keyboard.send_now();
+				Keyboard.set_key1(0);
+				Keyboard.send_now();
+			}
+			enclevel = newPosition;
+		}
+		oldPosition = newPosition;
+	}
 }
 
